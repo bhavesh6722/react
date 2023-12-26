edbaff3894dfef4510c6fa7e9b1e5c3edb78f013
bhavesh6722 <bhavesh2000solanki@gmail.com>
2023-12-26 10:03:09 +0530

    submit

---
+++ lazyloading/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+// import './App.css';
+
+
+import MyLazyComponent from './MyLazyComponent';
+
+
+
+function App() {
+  return (
+    <div>
+     <MyLazyComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lazyloading/src/MyLazyComponent.js
@@ -0,0 +1,19 @@
+import React, { lazy, Suspense } from 'react';
+
+
+const LazyReactLogo = lazy(()=>{return new Promise (resolve => {
+  setTimeout(() => {
+     resolve(import("./Como"))
+  }, 4000);
+})});
+
+const MyLazyComponent = () => (
+  <div style={{textAlign:"center"}}>
+    <h1>Welcome to My Lazy-loaded React Component!</h1>
+    <Suspense fallback={<h1>Waiting..........</h1>} >
+      <LazyReactLogo/>
+    </Suspense>
+  </div>
+);
+
+export default MyLazyComponent;
